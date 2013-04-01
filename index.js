8cce714c0c6e1d08e03b48397a3ae966f869372b
Robert Kowalski <rok@kowalski.gd>
2013-04-01 23:54:34 +0200

    rename module, bump version

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = process.env.TEST_COV
-  ? require('./lib-cov/connect-multiple-redis')
-  : require('./lib/connect-multiple-redis');
+  ? require('./lib-cov/connect-multi-redis')
+  : require('./lib/connect-multi-redis');
